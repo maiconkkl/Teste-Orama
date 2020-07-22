30d3751f5c7111cb1e68908458aef7a45e46f424
Michael Douglas <contatomichael@outlook.com.br>
2020-07-22 05:47:04 -0300

    ajuste na documentação

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 from collections import namedtuple
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restx import Resource, Api, reqparse, fields
 import json
 
 app = Flask(__name__)
@@ -44,30 +44,73 @@ def calcular_tempo(lista: list):
     return tempo
 
 
+parser = reqparse.RequestParser()
+parser.add_argument('id', type=int, default=0)
+parser.add_argument('status', type=str, default='seeking')
+parser.add_argument('startDate', type=str, default='2018-07-14T09:31:21+02:00')
+parser.add_argument('retributionWished', type=int, default=0)
+parser.add_argument('durationInDays', type=int, default=0)
+parser.add_argument('expertise_id', type=int, default=0)
+parser.add_argument('expertise_name', type=str, default='')
+parser.add_argument('skills', type=list, default=[])
+
+
+@api.route('/freelance')
 class Freelancer(Resource):
-    def get(self):
-        dados = {}
-        with open('orama/examples/freelancer.json', 'r') as outfile:
-            data = json.load(outfile)
-            # Vamos carregar as informações do freelances
-            for x in data['freelance']['professionalExperiences']:
-                if x['id'] not in dados:
-                    dados[x['id']] = {'skills': {}}
-                for y in x['skills']:
-                    if y['name'] not in dados[x['id']]['skills']:
-                        dados[x['id']]['skills'][y['name']] = []
-                    dados[x['id']]['skills'][y['name']].append({
-                        'start': datetime.datetime.strptime(x['startDate'], '%Y-%m-%dT%H:%M:%S%z'),
-                        'end': datetime.datetime.strptime(x['endDate'], '%Y-%m-%dT%H:%M:%S%z'),
-                        'somado': False
-                    })
+    @api.expect(parser, validate=True)
+    def post(self):
+        print(parser.parse_args())
+        try:
+            with open('examples/freelancer.json', 'r') as outfile:
+                data = {}
+                data_json = json.load(outfile)
+                # Vamos carregar as informações do freelances
+                for x in data_json['freelance']['professionalExperiences']:
+                    if x['id'] not in data:
+                        data[x['id']] = {'skills': {}}
+                    # Vamos coletar todas as habilidades
+                    for y in x['skills']:
+                        # Verificando se essa habilidade ja foi coletada
+                        if y['id'] not in data[x['id']]['skills']:
+                            data[x['id']]['skills'][y['id']] = {
+                                'id': y['id'],
+                                'name': y['name'],
+                                'dates': []
+                            }
 
-            for id, value in dados.items():
-                for skill, dates in value['skills'].items():
-                    print(calcular_tempo(dates))
-            return data
+                        # Coletando o tempo de serviço dessa habilidade
+                        data[x['id']]['skills'][y['id']]['dates'].append({
+                            'start': datetime.datetime.strptime(x['startDate'], '%Y-%m-%dT%H:%M:%S%z'),
+                            'end': datetime.datetime.strptime(x['endDate'], '%Y-%m-%dT%H:%M:%S%z'),
+                            'somado': False
+                        })
+
+                # Vamos prepara os dados de retorno
+                data_return = {
+                    "freelance": []
+                }
+                for id, value in data.items():
+                    freelance = {
+                        'id': id,
+                        "computedSkills": []
+                    }
+
+                    # Antes de prosseguir, vamos calcular o tempo de serviço
+                    for skill_id, value_skill in value['skills'].items():
+                        skill_return = {
+                            "id": value_skill['id'],
+                            "name": value_skill['name'],
+                            "durationInMonths": int(calcular_tempo(value_skill['dates']) / 30)
+                        }
+                        freelance['computedSkills'].append(skill_return)
+
+                    data_return['freelance'].append(freelance)
+
+                return data_return
+        except:
+            content = {'error': 'payload is invalid'}
+            return content, 422
 
 
-api.add_resource(Freelancer, '/')
 if __name__ == '__main__':
     app.run(debug=True)
